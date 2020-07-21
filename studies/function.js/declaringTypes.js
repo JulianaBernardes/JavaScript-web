489a2ce76c17d34e45e115a4d0b72584fe0c1c41
Juliana Bernardes Pereira <julianabp96@gmail.com>
2020-07-21 09:17:37 +0100

    commit

---
+++ studies/function.js/declaringTypes.js
@@ -0,0 +1,17 @@
+console.log(sum(3, 4))
+//console.log(multi(3, 4)) //not initializated
+
+// function declaration
+function sum(x, y) {
+    return x + y
+}
+    
+//functuon expression
+const sub = function (x, y) {
+    return x - y
+}
+
+// named function expression (less used in JS)
+    const mult = function mult(x, y) {
+    return x * y
+}
---
+++ studies/function.js/factory1.js
@@ -0,0 +1,9 @@
+// Simple factory
+function createPeople() {
+    return {
+        name: 'Ana',
+        surname: 'Silva'
+    }
+}
+
+console.log(createPeople())
