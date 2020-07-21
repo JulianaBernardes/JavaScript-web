ba77fd4a12a057e51c313e4dba26592dffc31d16
Juliana Bernardes Pereira <julianabp96@gmail.com>
2020-07-21 16:44:24 +0100

    commit

---
+++ studies/exercises-functions/ex01.js
@@ -0,0 +1,9 @@
+function expression (num1, num2) {
+    sum = num1 + num2
+    subt = num1 - num2
+    multi = num1 * num2
+    divi = num1 / num2
+    
+    console.log(sum,subt,multi,divi)    
+}
+expression(2,4)
---
+++ studies/exercises-functions/ex03.js
@@ -0,0 +1,4 @@
+(function(num1,num2) {
+    exp = num1**num2
+    console.log(exp)
+})(2,4)
