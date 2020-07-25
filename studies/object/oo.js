4bd58c55232fb1bac780f360c220e5b45381e21f
Juliana Bernardes Pereira <julianabp96@gmail.com>
2020-07-25 09:33:10 +0100

    commit

---
+++ studies/object/oo.js
@@ -0,0 +1,22 @@
+// non-executable code
+
+//procedural
+processing(value1, value2, value3)
+
+//OO
+object = {
+    value1,
+    value2,
+    value3,
+    processing() {
+        //...
+    }
+}
+
+object.processing()
+
+// Important principles
+//1) Abstraction
+//2) Encapsulation
+//3) Inheritance (prototype)
+//4) Polymorphism
