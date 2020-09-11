61b22b9dc2faeca63060388b4825c713c30502c7
Juliana Bernardes Pereira <julianabp96@gmail.com>
2020-09-11 09:47:29 +0100

    AJAX with jquery

--- studies/ajax/server.js
+++ studies/ajax/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('.'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 const multer = require('multer')
@@ -17,14 +17,13 @@ const storage = multer.diskStorage({
     }
 })
 
-const upload = multer({storage}).single('file')
+const upload = multer({ storage }).single('file')
 
 app.post('/upload', (req, res) => {
     upload(req, res, err => {
-        if(err) {
+        if (err) {
             return res.end('An error has occurred.')
         }
-
         res.end('Successfully concluded.')
     })
 })
@@ -36,10 +35,10 @@ app.post('/form', (req, res) => {
     })
 })
 
-app.get('/evenOrOdd/', (req, res) => {
-    // req.body
-    // req.query
-    // req.params
+app.get('/evenOrOdd', (req, res) => {
+    //req.body
+    //req.query
+    //req.params
     const even = parseInt(req.query.number) % 2 === 0
     res.send({
         result: even ? 'even' : 'odd'
