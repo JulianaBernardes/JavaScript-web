881652d3056c50096a3238de6183fabfda2f527d
Juliana Bernardes Pereira <julianabp96@gmail.com>
2020-09-24 09:34:36 +0100

    Bootstrap tooltip

--- studies/bootstrap/exercises/js/navigation.js
+++ studies/bootstrap/exercises/js/navigation.js
@@ -12,6 +12,7 @@
             .then(resp => resp.text())
             .then(html => {
                 destiny.innerHTML = html
+                eval(html.match(/\<script\>([\s\S]*)\<\/script\>/)[1])
             })
     }
 
