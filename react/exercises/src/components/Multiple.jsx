9c1fa9ddc71f0589e75aff83733d8a24efa43920
Juliana Bernardes Pereira <julianabp96@gmail.com>
2020-10-01 09:29:58 +0100

    React Class components

---
+++ react/exercises/src/components/Multiple.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GoodAfternoon = props => <h1>Good afternoon {props.name}!</h1>
+
+const GoodNight = props => <h1>Good Night {props.name}!</h1>
+
+export {GoodAfternoon, GoodNight}
+
+export default { GoodAfternoon, GoodNight}
--- react/exercises/src/index.js
+++ react/exercises/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import GoodMorning from './components/GoodMorning'
+// import GoodMorning from './components/GoodMorning'
+import  Salute from './components/Salute'
 
-ReactDOM.render(<GoodMorning name="Junior" />, document.getElementById('root'))
+
+ReactDOM.render(
+    <div>
+        <Salute type="Good Morning" name="Junior" />
+    </div>
+    , document.getElementById('root'))
 
